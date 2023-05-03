adf85ce58df1e18f08a9a3c85aa4351f871ccf0f
Taksh Kothari <96124613+takshkothari@users.noreply.github.com>
2023-05-04 00:13:26 +0530

    need to put values

---
+++ relations.sql
@@ -0,0 +1,29 @@
+delete from student;
+delete from course;
+delete from assignment_exam;
+delete from enrolls;
+delete from takes;
+delete from belongs;
+delete from sem;
+
+insert into student values ();
+insert into student values ();
+
+insert into course values ();
+insert into course values ();
+
+insert into assignment_exam values ();
+insert into assignment_exam values ();
+insert into assignment_exam values ();
+
+insert into enrolls values ();
+insert into enrolls values ();
+
+insert into takes values ();
+insert into takes values ();
+
+insert into belongs values ();
+insert into belongs values ();
+
+insert into sem values ();
+insert into sem values ();
