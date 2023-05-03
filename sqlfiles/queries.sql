b7f6e1ac83b9b04c7a53bc5f2058562db09a3535
takshkothari <96124613+takshkothari@users.noreply.github.com>
2023-05-04 01:07:27 +0530

    initial queries

--- sqlfiles/queries.sql
+++ sqlfiles/queries.sql
@@ -1,4 +1,15 @@
 /* to register user */
 insert into student values ();
 
-/* adding cou
+/* see profile */
+/* general */
+select regNo, stud_name, sem, dob, email, phone
+from student natural join sem
+where stud_name = <login details> and sem = <selected sem>;
+/* update email button */
+update student set email = <new email>
+where stud_name = <login details> and sem = <selected sem>;
+/* update phone button */
+update student set phone = <new phone>
+where stud_name = <login details> and sem = <selected sem>;
+
