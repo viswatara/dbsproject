e50c300b4780f182f8b5d167adc66b4529a7fad0
viswatara <99180943+viswatara@users.noreply.github.com>
2023-05-12 11:38:19 +0530

    Update createtable.sql

--- sqlfiles/createtable.sql
+++ sqlfiles/createtable.sql
@@ -8,3 +8,17 @@ CREATE TABLE student (
   PRIMARY KEY (regNo),
   CONSTRAINT email_check CHECK (email LIKE '%_@_____%___%')
 );
+
+
+CREATE TABLE course (
+  course_id varchar(8) primary key,
+  c_title varchar(20) not null,
+  credit numeric(1,0) check (credit > 0)
+);
+CREATE TABLE assignment (
+  ae_id varchar(10) NOT NULL,
+  maxmarks numeric(2,0) NOT NULL check (maxmarks<100),
+  course_id varchar(8),
+  PRIMARY KEY (ae_id),
+  FOREIGN KEY (course_id) REFERENCES course(course_id) ON UPDATE CASCADE
+);
